318a187c80f17cf4c7d5f5041643fdb7471b8be5
Melanie Escobar <157929578+Ephimoon@users.noreply.github.com>
2024-10-07 01:54:56 -0500

    added register page

---
+++ routes/routes/Register.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+
+// Add your route handlers here
+router.post('/register', (req, res) => {
+  // Logic for handling register
+});
+
+export default router;
--- server.js
+++ server.js
@@ -16,28 +16,38 @@ app.use(express.json());  // To parse JSON data
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Import the routes
-import volunteerHistoryRoutes from './routes/routes/volunteerHistory.js'; // Gaby
-import LoginRoute from './routes/routes/Login.js'; // Mel
-import VolunteerDashboardRoute from './routes/routes/VolunteerDashboard.js'; // Mel
+// Gaby
+import volunteerHistoryRoutes from './routes/routes/volunteerHistory.js';
+// Mel
+import LoginRoute from './routes/routes/Login.js';
+import RegisterRoute from './routes/routes/Register.js';
+import VolunteerDashboardRoute from './routes/routes/VolunteerDashboard.js';
 // Syeda
 // Angie
 
 // Use the routes
-app.use('/api/volunteer-history', volunteerHistoryRoutes); // Gaby
-app.use('/api/login', LoginRoute); // Mel
-app.use('/api/volunteer-dashboard', VolunteerDashboardRoute); // Mel
+// Gaby
+app.use('/api/volunteer-history', volunteerHistoryRoutes);
+// Mel
+app.use('/api/login', LoginRoute);
+app.use('/api/register', RegisterRoute);
+app.use('/api/volunteer-dashboard', VolunteerDashboardRoute);
 // Syeda
 // Angie
 
 // Placeholder routes for other pages
 app.get('/', (req, res) => res.json({ message: 'Welcome to the Home page!' }));
-//app.get('/api/login', (req, res) => res.json({ message: 'Login page route' }));
-app.get('/api/register', (req, res) => res.json({ message: 'Register page route' }));
+// Gaby
 app.get('/api/admin-dashboard', (req, res) => res.json({ message: 'Admin Dashboard page route' }));
-//app.get('/api/volunteer-dashboard', (req, res) => res.json({ message: 'Volunteer Dashboard page route' }));
-app.get('/api/volcards', (req, res) => res.json({ message: 'Volunteer Matching Form page route' }));
+// Mel
+// app.get('/api/login', (req, res) => res.json({ message: 'Login page route' }));
+// app.get('/api/register', (req, res) => res.json({ message: 'Register page route' }));
+// app.get('/api/volunteer-dashboard', (req, res) => res.json({ message: 'Volunteer Dashboard page route' }));
+// Syeda
 app.get('/api/volunteermanagmentform', (req, res) => res.json({ message: 'Volunteer Management Form page route' }));
 app.get('/api/eventmanagmentform', (req, res) => res.json({ message: 'Event Management Form page route' }));
+// Angie
+app.get('/api/volcards', (req, res) => res.json({ message: 'Volunteer Matching Form page route' }));
 app.get('/api/notifs', (req, res) => res.json({ message: 'Notifications page route' }));
 
 // Set up the server
