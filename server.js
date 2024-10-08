7473538b57306e895ac3ead207921550ac515ec1
Gab <gabe.rome2020@gmail.com>
2024-10-07 20:50:05 -0500

    Modified routes/routes to just be a route folder

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());  // To parse JSON data
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Import the routes
-import volunteerHistoryRoutes from './routes/routes/volunteerHistory.js'; // Gaby
-import LoginRoute from './routes/routes/Login.js'; // Mel
-import VolunteerDashboardRoute from './routes/routes/VolunteerDashboard.js'; // Mel
+import volunteerHistoryRoutes from './routes/volunteerHistory.js'; // Gaby
+import LoginRoute from './routes/Login.js'; // Mel
+import VolunteerDashboardRoute from './routes/VolunteerDashboard.js'; // Mel
 // Syeda
 // Angie
 
