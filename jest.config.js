40555aa4a51b3c537e78ff8d4a2638309435d104
Angela-Marie Abrea <165529511+angieluvss@users.noreply.github.com>
2024-10-19 20:55:57 -0500

    changes to pass test cases with notifs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    transform: {
-      '^.+\\.js$': 'babel-jest',  // If using Babel
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'], // Ensure this is correctly set
-    moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'], // For various file types
-  };
-  
+  transform: {
+    '^.+\\.js$': 'babel-jest',  // Use babel-jest to transform JS files
+  },
+  testEnvironment: 'node',  // Set the test environment to Node.js
+};
